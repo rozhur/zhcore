612836b09bec46691b70d5c4edcd2256f27f3ef5
Roman Zhuravlev <rozhur@zhira.net>
2025-10-20 07:14:51 +0500

    Change site description

--- template/global_sidebar.php
+++ template/global_sidebar.php
@@ -2,13 +2,9 @@
     <div style="text-align: center">
     <img src="https://dweb.by/images/services/website_layout/website_layout.png"
          style="width: 100%; max-width: 200px"/></div>
-    <h4>Услуги</h4>
-    <ul class="list">
-        <li>создание сайтов с нуля (PHP, MySQL, JS, CSS, HTML)</li>
-        <li>верстка шаблонов (стилей) для XenForo</li>
-        <li>Java: написание плагинов для серверных ядер Minecraft (Bukkit, Spigot, PaperSpigot и др.)</li>
-    </ul>
+    <h4>О сайте</h4>
+    <p>Мессенджер с функцией мгновенного обмена сообщениями.</p>
 </div>
 <div class="block" style="text-align: center">
-    <a href="https://vk.com/rozhur" class="button button--feature" style="width: 100%" target="_blank">VK</a>
+    <a href="https://github.com/rozhur/zhcore" class="button button--feature" style="width: 100%" target="_blank">GitHub</a>
 </div>
